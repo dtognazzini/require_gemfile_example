5ed2d4c06dc275362331f8c1e07b846780c4e929
Donnie Tognazzini <donnie.tognazzini@appfolio.com>
2014-11-06 09:58:49 -0800

    adding first engine

---
+++ engines/blorgh/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '5ec7ba3e93cb0b10939e8e9e0119629a98fb3531b3fbf8feb1ef3824bb2f4b203c23e691322f89d2781d6535e8b33dde4b28b71328444067b15fa24fa1ee61cf'
